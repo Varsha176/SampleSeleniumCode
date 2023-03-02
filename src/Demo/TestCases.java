0050d096627e6838e3d20f9e302f8c49b2805b10
varsha rajput <varsha130893@gmail.com>
2023-03-02 13:02:40 +0530

    Update TestCases.java

--- src/Demo/TestCases.java
+++ src/Demo/TestCases.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
