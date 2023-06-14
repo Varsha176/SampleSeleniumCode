d7656aab9adff9a3d9f4f9086cc16177b8b6d658
varsha rajput <varsha130893@gmail.com>
2023-06-14 15:52:35 +0530

    Update TestCases.java

--- src/Demo/TestCases.java
+++ src/Demo/TestCases.java
@@ -17,7 +17,13 @@ public class TestCases {
 
 	@BeforeTest
 	public void setup() {
-		
+		ChromeOptions options = new ChromeOptions();
+
+ options.addArguments("--no-sandbox");
+
+ options.addArguments("--disable-dev-shm-usage");
+
+ options.addArguments("--headless");
 		WebDriverManager.chromedriver().setup();
 	 driver = new ChromeDriver();
 		 Reporter.log("We used Google Chrome for this test");
